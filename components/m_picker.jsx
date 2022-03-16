c170d0030e50b87fa79a40dd13b391f201c0cfcd
Milo Woodman <holymolyitsmelogurk@gmail.com>
2022-03-16 16:05:51 -0400

    funky mpicker UI, no functionality though

--- components/m_picker.jsx
+++ components/m_picker.jsx
@@ -1,25 +1,54 @@
 import { StyleSheet, Text, View, Button, TouchableOpacity} from 'react-native';
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+    master: {
+        marginLeft: '25%',
+        marginTop: '5%',
+        height: '50%',
+        width: '100%'
+    },
+    on: {
+        backgroundColor: '#888',
+    },
+    off: {
+        backgroundColor: '#444'
+    },
+    textOff: {
+        color: '#faa',
+        marginBottom: '2%',
+        marginTop: '2%',
+        textAlign: 'center',
+        fontSize: 20
+    },
+    textOn: {
+        marginBottom: '2%',
+        marginTop: '2%',
+        color: '#afa',
+        textAlign: 'center',
+        fontSize: 20
+    }
+});
 
 export default function MPicker(props){
     return (
         <View style={styles.master}>
-            <TouchableOpacity>
-                <Text>{props.labels[0]}</Text>
-            </TouchableOpacity>
-            <TouchableOpacity>
-                <Text>{props.labels[1]}</Text>
-            </TouchableOpacity>
-            <TouchableOpacity>
-                <Text>{props.labels[2]}</Text>
-            </TouchableOpacity>
-            <TouchableOpacity>
-                <Text>{props.labels[3]}</Text>
-            </TouchableOpacity>
-            <TouchableOpacity>
-                <Text>{props.labels[4]}</Text>
-            </TouchableOpacity>
+            <View>
+                <TouchableOpacity style={styles.on}>
+                    <Text style={styles.textOn}>{props.labels[0]}</Text>
+                </TouchableOpacity>
+                <TouchableOpacity style={styles.off}>
+                    <Text style={styles.textOff}>{props.labels[1]}</Text>
+                </TouchableOpacity>
+                <TouchableOpacity style={styles.off}>
+                    <Text style={styles.textOff}>{props.labels[2]}</Text>
+                </TouchableOpacity>
+                <TouchableOpacity style={styles.off}>
+                    <Text style={styles.textOff}>{props.labels[3]}</Text>
+                </TouchableOpacity>
+                <TouchableOpacity style={styles.off}>
+                    <Text style={styles.textOff}>{props.labels[4]}</Text>
+                </TouchableOpacity>
+            </View>
         </View>
     );
 }
